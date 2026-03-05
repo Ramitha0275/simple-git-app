a7c2ed7d24583b3208e65abd34684e68c8e83223
Ramitha0275 <ramitha.s2023@vitstudent.ac.in>
2026-03-05 23:18:35 +0530

    Refactor addition logic to use command line arguments

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-a=int(input("Enter number a"))
-b=int(input("Enter number b"))
-c=a+b
-print(f"{c}")
+import sys
+def add(a,b):
+    return a+b
+if _name_=="main":
+    m=int(sys.argv[1])
+    n=int(sys.argv[2])
+    result=add(m,n)
+    print(f"{result}")
