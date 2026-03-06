9f07886b7461133acc841deb718ccacb933d2c75
Ramitha0275 <ramitha.s2023@vitstudent.ac.in>
2026-03-06 11:58:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,40 @@ pipeline {
  }
  }
 }
+
+git config --global user.name "Ramitha0275"
+git config --global user.email "ramitha.s2023@vitstudent.ac.in"
+
+mkdir git-experiment
+cd git-experiment
+git init
+echo 'print("Hello World")' > app.py
+git status
+git add app.py
+git commit -m "Initial commit: Added greeting application"
+
+git remote add origin https://github.com/Ramitha0275/simple-gitapp.git
+git branch -M main
+git push -u origin main
+
+echo 'print("Hello Worlds")' > app.py
+git checkout -b feature/add-greeting
+git add app.py
+git commit -m "Added additional greeting message"
+git checkout main
+git merge feature/add-greeting
+git push origin main
+
+git branch
+git branch -d feature/add-greeting
+
+git clone https://github.com/Sairamitha/simple-git-app.git
+cd simple-git-app
+
+git checkout -b feature/your-name
+echo 'print("Hello Worldsyyyyy")' > app.py
+git add app.py
+git commit -m "Member contribution: Added specific feature"
+
+git checkout main
+git pull origin main
